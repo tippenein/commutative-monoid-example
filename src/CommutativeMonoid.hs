e125a3ca2e5a67db3099713adf247474b6e07cf0
brady.ouren <brady.ouren@gmail.com>
2016-06-06 14:47:31 -0700

    top10 aggregation - CollectionTops data type

--- src/CommutativeMonoid.hs
+++ src/CommutativeMonoid.hs
@@ -5,8 +5,13 @@ import Data.Monoid
 
 data Collection = Collection
   { theSum :: Integer
-  , count  :: Int
+  , count  :: Integer
   , avg    :: Maybe Double
+  } deriving (Show, Eq)
+
+data CollectionTops = CollectionTops
+  { aSum :: Integer
+  , tops :: [Integer]
   } deriving (Show, Eq)
 
 instance Monoid Collection where
@@ -18,5 +23,16 @@ instance Monoid Collection where
   (Collection sum1 count1 (Just avg1)) `mappend` (Collection sum2 count2 (Just avg2)) =
     Collection (sum1 + sum2) (count1 + count2) average
     where
-      average = pure $ realToFrac (avg1 + avg2) / 2
+      average = Just $
+        ((avg1 * fromInteger count1) + (avg2 * fromInteger count2)) /
+          (fromInteger count1 + fromInteger count2)
   mconcat = foldl' mappend mempty
+
+negativeInfinities = replicate 10 (-1)
+revSort = sortBy (flip compare)
+instance Monoid CollectionTops where
+  mempty = CollectionTops 0 negativeInfinities
+  CollectionTops s1 tops1 `mappend` CollectionTops s2 tops2 =
+    CollectionTops (s1 + s2) (take 10 $ revSort ([s1] ++ [s2] ++ tops1 ++ tops2))
+  mconcat = foldl' mappend mempty
+
--- test/CommutativeMonoidSpec.hs
+++ test/CommutativeMonoidSpec.hs
@@ -1,26 +1,36 @@
 module CommutativeMonoidSpec (spec) where
 
 import CommutativeMonoid
+import Prelude hiding (replicate, take)
 import Test.Hspec
 import Test.QuickCheck
 
 import Control.Monad (when)
 import Data.Monoid
 
-cols1 = [collectionFrom 5 1 5, collectionFrom 4 1 4, collectionFrom 5 1 5]
+cols1 = [collectionFrom 5 1 5, collectionFrom 4 1 4, collectionFrom 6 1 6]
 allOnes = replicate 10 (collectionFrom 1 1 1)
 
 collectionFrom s c a = if a > 0 then Collection s c (Just a) else Collection s c Nothing
 
+take :: Integer -> [a] -> [a]
+take n _      | n <= 0 =  []
+take _ []              =  []
+take n (x:xs)          =  x : take (n-1) xs
+
+replicate :: Integer -> a -> [a]
+replicate n x = take n (repeat x)
+
+topsCollection1 = CollectionTops 1 (reverse [1..10])
 spec :: Spec
-spec =
-  describe "main" $ do
+spec = do
+  describe "collection" $ do
     it "averages a composition of 2 Collections" $ do
       (collectionFrom 1 1 1 <> collectionFrom 10 1 10) `shouldBe` collectionFrom 11 2 5.5
-      (collectionFrom 11 2 5.5 <> collectionFrom 3 1 3) `shouldBe` collectionFrom 14 3 4.25
+      (collectionFrom 11 2 5.5 <> collectionFrom 4 1 4) `shouldBe` collectionFrom 15 3 5.0
 
     it "averages a list of Collections" $ do
-      mconcat cols1 `shouldBe` collectionFrom 14 3 4.75
+      mconcat cols1 `shouldBe` collectionFrom 15 3 5.0
       mconcat allOnes `shouldBe` collectionFrom 10 10 1
 
     it "counts positive values consistently" $ property $ \n ->
@@ -28,3 +38,13 @@ spec =
 
     it "disallows negative values" pending
       -- (mconcat [Collection 1 1 (-1)]) `shouldThrow` anyException
+
+  describe "collectionTops" $ do
+    it "collects the top10" $
+      topsCollection1 <> mempty `shouldBe` topsCollection1
+
+    it "collects the top10" $ do
+      let cs = topsCollection1 <> CollectionTops 3000 negativeInfinities
+      head (tops cs) `shouldBe` 3000
+      (1 `elem` tops cs) `shouldBe` False
+
